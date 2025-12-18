8d51f21400e366b58c0ad5f3e8d26429006cf085
niangao233 <115617733+niangao233@users.noreply.github.com>
2025-12-18 16:38:48 +0800

    1

--- docs/sync-issue-from-md.js
+++ docs/sync-issue-from-md.js
@@ -6,7 +6,7 @@ const path = require('path');
 async function run() {
   try {
     // ==================== 1. 初始化 ====================
-    const token = process。env.GITHUB_TOKEN;
+    const token = process.env.GITHUB_TOKEN;
     const octokit = github.getOctokit(token);
     const context = github.context;
 
